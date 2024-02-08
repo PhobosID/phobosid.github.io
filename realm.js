f0438e712041c0818e75abc0592f6eb0f01fbb8b
Kristoff Verdenberg <service.retrieval688@passinbox.com>
2024-02-08 20:48:01 +0700

    Update Realm

--- realm.js
+++ realm.js
@@ -1,6 +1,7 @@
+// You Got Me, Yes that is the Password lol. Say I handsome now. No G4Y tho.
 function validatePassword() {
     const enteredPassword = document.getElementById('password').value;
-    if (enteredPassword === 'satujamlimaribu') {
+    if (enteredPassword === 'pobiganteng') {
       return true;
     } else {
       alert('Wrong Password! Please Try Again.');
